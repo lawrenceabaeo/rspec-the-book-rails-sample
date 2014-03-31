cabc7fce6d8ec260c171c7eb55c656b9225aa22a
Lawrence Abaeo <labaeo@gmail.com>
2014-03-31 16:04:52 -0700

    Write step definitions for showtime feature. Create movie model.

--- features/step_definitions/showtime_steps.rb
+++ features/step_definitions/showtime_steps.rb
@@ -4,7 +4,7 @@ end
 
 When /^I set the showtime to "([^"]*)" at "([^"]*)"$/ do |date, time|
   @movie.update_attribute(:showtime_date, Date.parse(date))
-  @movie.update_attribute(:showtime_time, Time)
+  @movie.update_attribute(:showtime_time, time)
 end
 
 Then /^the showtime description should be "([^"]*)"$/ do |showtime|
